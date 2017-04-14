f213f3703513ac2d502f30af136dc609a1367923
YousefMohsen <Alkhozai@Yousefs-iMac.local>
2017-04-15 00:54:40 +0200

    added pictures

--- web/scripts/playVideoScript.js
+++ web/scripts/playVideoScript.js
@@ -5,10 +5,97 @@
  */
 
 
+
+/* pop up video*/
+/*
+$(document).foundation();
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '315',
+        width: '560',
+        videoId: 'l-gQLqv9f4o',
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+      //player is playing
+    } else {
+      //player is paused
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+function playVideo() {
+  player.playVideo();
+}
+
+function pauseVideo() {
+  player.pauseVideo();
+}
+
+
+$(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
+    playVideo();
+});
+
+$(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
+  pauseVideo();
+});
+
+
+/* pop up video*/
+
+
+/*
+
+
+
 playBtn = document.getElementById("playBtn");
+playBtn1 = document.getElementById("playBtn1");
 replayBtn = document.getElementById("replay-Btn");
   carImg  = document.getElementById("carImg"); 
   carVideo = document.getElementById("carVideo"); 
+
+
+
+
+
+
+
+playBtn1.addEventListener("click",function(){
+
+
+
+    
+});
+
+
+
+
+
+
+
+
+
 
 carImg.addEventListener("click",function(){
     carImg.style.display = "none";
@@ -50,3 +137,7 @@ var showButton = function(){
      carVideo.style.display = "none";  
    
 };
+
+
+
+*/
