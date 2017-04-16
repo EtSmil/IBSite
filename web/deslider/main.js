b526b690ab88a7730a5b95a894291631d9a76ffd
YousefMohsen <Alkhozai@Yousefs-iMac.local>
2017-04-16 13:36:11 +0200

    videoSection

--- web/deslider/main.js
+++
@@ -1,8 +0,0 @@
-/*
-	http://ccoenraets.github.io/es6-tutorial/classes/
-	http://stackoverflow.com/questions/37912857/exporting-a-class-with-webpack-and-babel-not-working
- */
-
-// require("../css/style.scss");
-import Deslider from './deslider';
-module.exports = Deslider;
